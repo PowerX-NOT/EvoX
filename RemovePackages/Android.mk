89c4ebbbba849128e0419a83f08c6dfeb5df8590
NOT_SURE <101414049+PowerX-NOT@users.noreply.github.com>
2023-04-08 23:35:50 +0530

    laurel_sprout: updated RemovePackages (gms)

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -4,154 +4,28 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt Camera2 CarrierSetup
-LOCAL_OVERRIDES_PACKAGES += MyVerizonServices Music
-LOCAL_OVERRIDES_PACKAGES += OBDM_Permissions OPScreenRecord RecorderPrebuilt
-LOCAL_OVERRIDES_PACKAGES += SafetyHubPrebuilt SprintDM SprintHM
-LOCAL_OVERRIDES_PACKAGES += VzwOmaTrigger YouTube YouTubeMusicPrebuilt
 LOCAL_OVERRIDES_PACKAGES := \
+	ANGLE \
+	BetterBugStub \
+	AmbientSensePrebuilt \
+	AppDirectedSMSService \
+	CalculatorGooglePrebuilt \
 	CalendarGooglePrebuilt \
 	Camera2 \
 	Camera2Stub \
-	Drive \
-	Maps \
-	FilesPrebuilt \
-	OBDM_Permissions \
-	SafetyHubPrebuilt \
-	SprintDM \
-	SprintHM \
-	USCCDM \
-	VZWAPNLib \
-	VzwOmaTriger \
-        PixelBuds \
-        PixelLiveWallpaperPrebuilt \
-        RecorderPrebuilt \
-        SafetyHubPrebuilt \
-        SCONE \
-        ScribePrebuilt \
-        Showcase \
-        SoundAmplifierPrebuilt \
-        SprintDM \
-        SprintHM \
-        Tycho \
-        USCCDM \
-        Via \
-        Videos \
-        VZWAPNLib \
-        VzwOmaTrigger \
-        YouTube \
-        YouTubeMusicPrebuilt \
-        AmbientSensePrebuilt \
-        arcore \
-        ConnMO \
-        ConnMetrics \
-        DCMO \
-        DevicePolicyPrebuilt \
-        DMService \
-        GCS \
-        GoogleTTS \
-        Maps \
-        MyVerizonServices \
-        OBDM_Permissions \
-        obdm_stub \
-        OdadPrebuilt \
-        OemDmTrigger \
-        OPScreenRecord \
-        Ornament \
-        PixelBuds \
-        PixelLiveWallpaperPrebuilt \
-        RecorderPrebuilt \
-        SafetyHubPrebuilt \
-        SCONE \
-        ScribePrebuilt \
-        Showcase \
-        SoundAmplifierPrebuilt \
-        SprintDM \
-        SprintHM \
-        Tycho \
-        USCCDM \
-        Via \
-        Videos \
-        VZWAPNLib \
-        VzwOmaTrigger \
-        YouTube \
-        YouTubeMusicPrebuilt \
-        AmbientSensePrebuilt \
-	AppDirectedSMSService \
 	CalendarGooglePrebuilt \
-	Camera2 \
-	CarrierSetup \
-	ConnMO \
-	DCMO \
-	DMService \
-	DevicePolicyPrebuilt \
-	DiagnosticsToolPrebuilt \
-	Drive \
-	HelpRtcPrebuilt \
-	Maps \
-	MyVerizonServices \
-	NgaResources \
-	OBDM_Permissions \
-	OemDmTrigger \
-	PixelLiveWallpaperPrebuilt \
-	PixelWallpapers2021 \
-	PrebuiltGmail \
-	RecorderPrebuilt \
-	SafetyHubPrebuilt \
-	ScribePrebuilt \
-	Showcase \
-	SoundAmplifierPrebuilt \
-	SprintDM \
-	SprintHM \
-	Tycho \
-	USCCDM \
-	VZWAPNLib \
-	VzwOmaTriger \
-        AmbientSensePrebuilt \
-        arcore \
-        ConnMO \
-        ConnMetrics \
-        DCMO \
-        DevicePolicyPrebuilt \
-        DMService \
-        GCS \
-        GoogleTTS \
-        Maps \
-        MyVerizonServices \
-        OBDM_Permissions \
-        obdm_stub \
-        OdadPrebuilt \
-        OemDmTrigger \
-        OPScreenRecord \
-        Ornament \
-        PixelBuds \
-        PixelLiveWallpaperPrebuilt \
-        RecorderPrebuilt \
-        SafetyHubPrebuilt \
-        SCONE \
-        ScribePrebuilt \
-        Showcase \
-        SoundAmplifierPrebuilt \
-        SprintDM \
-        SprintHM \
-        Tycho \
-        USCCDM \
-        Via \
-        Videos \
-        VZWAPNLib \
-        VzwOmaTrigger \
-        YouTube \
-        YouTubeMusicPrebuilt \
-    CalculatorGooglePrebuilt \
-    CalendarGooglePrebuilt \
+	CarrierLocation \
+	DeviceIntelligenceNetworkPrebuilt \
+	PlayAutoInstallConfig \
+	Velve \
     Chrome-Stub \
-    GoogleContacts \
     WebViewGoogle \
     WebViewGoogle-Stub \
     arcore \
-    AndroidAutoStubPrebuilt \
     talkback \
-                    
+    SoundPickerPrebuilt \
+    TrichromeLibrary \
+    TrichromeLibrary-Stub   
 
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
