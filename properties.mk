94ee48cdfe416e2d7cfd676c1b0d7e83a9bc1dc1
PowerX-NOT <pugalg100@gmail.com>
2023-04-18 14:05:16 +0530

    laurel_sprout: Enable debug.sf.auto_latch_unsignale

--- properties.mk
+++ properties.mk
@@ -160,6 +160,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.disable_client_composition_cache=1 \
     debug.sf.layer_caching_active_layer_timeout_ms=1000 \
     debug.sf.enable_gl_backpressure=1 \
+    debug.sf.auto_latch_unsignaled=true \
     ro.opengles.version=196610 \
     ro.hardware.egl=adreno \
     ro.hardware.vulkan=adreno \
