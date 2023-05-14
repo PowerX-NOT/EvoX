39f2a9d9c879fb840f79a748e349cbbccdfb6853
PowerX-NOT <pugalg100@gmail.com>
2023-05-14 12:07:12 +0530

    laurel_sprout: nuked DISPPARAM_PATH

--- fingerprint/BiometricsFingerprint.cpp
+++ fingerprint/BiometricsFingerprint.cpp
@@ -34,10 +34,6 @@
 #define PARAM_NIT_630_FOD 1
 #define PARAM_NIT_NONE 0
 
-#define DISPPARAM_PATH "/sys/class/drm/card0-DSI-1/disp_param"
-#define DISPPARAM_HBM_FOD_ON "0x1d20FE0"
-#define DISPPARAM_HBM_FOD_OFF "0x20f0F20"
-
 #define FOD_STATUS_PATH "/sys/class/touch/tp_dev/fod_status"
 #define FOD_STATUS_ON 1
 #define FOD_STATUS_OFF -1
@@ -482,7 +478,6 @@ Return<bool> BiometricsFingerprint::isUdfps(uint32_t /* sensorId */) {
 Return<void> BiometricsFingerprint::onFingerDown(uint32_t /* x */, uint32_t /* y */,
                                                 float /* minor */, float /* major */) {
     if (mHaveUdfps) {
-    set(DISPPARAM_PATH, DISPPARAM_HBM_FOD_ON);
     set(FOD_STATUS_PATH, FOD_STATUS_ON);
     }
     return Void();
@@ -490,7 +485,6 @@ Return<void> BiometricsFingerprint::onFingerDown(uint32_t /* x */, uint32_t /* y
 
 Return<void> BiometricsFingerprint::onFingerUp() {
     if (mHaveUdfps) {
-    set(DISPPARAM_PATH, DISPPARAM_HBM_FOD_OFF);
     set(FOD_STATUS_PATH, FOD_STATUS_OFF);
    }
     return Void();
